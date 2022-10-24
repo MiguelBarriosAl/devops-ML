484ac0cb60c5d64df5ae990402069cee5d12a6b7
Miguel Barrios <mbarrios@enthec.com>
2022-10-25 01:28:10 +0200

    Test_model_deployed test_heathcheck_api

---
+++ test/test_heathcheck_api.py
@@ -0,0 +1,18 @@
+import requests
+import unittest
+from starlette import status
+
+
+class TestModel(unittest.TestCase):
+
+    def test_get_status_code_equals_200(self):
+        # docker run -p 80:80 tensor-prediction
+        response = requests.get("http://localhost:80")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test/test_model_deployed.py
@@ -0,0 +1,16 @@
+import unittest
+import requests
+from fastapi import status
+
+
+class TestModel(unittest.TestCase):
+
+    def test_model_deployed(self):
+        # docker run -p 80:80 tensor-prediction
+        data = {"tensor": [0, 1, 2]}
+        headers = {"content-type": "application/json"}
+        response = requests.post(("http://localhost:80/predict"), json=data, headers=headers)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+if __name__ == '__main__':
+    unittest.main()
